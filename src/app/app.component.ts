4b67cc98ce55eb6ea5f133c9c768dae9c8616117
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-11 21:13:40 +0100

    Add button to template and code to matching component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  handleClick(){
+    console.log('Someone clicked on button on the template :-)')
+  }
 }
